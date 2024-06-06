43e487343b7631a4fd045608bede182a4956efee
hiimo <115462482+hiimo@users.noreply.github.com>
2024-06-07 01:17:35 +0300

    Update yl4cv.php

--- yl4cv.php
+++ yl4cv.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>Title</title>
+        <title>Avaleht</title>
         <meta charset="utf-8" />
         <meta
             name="viewport"
@@ -18,68 +18,26 @@
         <script src="https://kit.fontawesome.com/5f11feec13.js" crossorigin="anonymous"></script>
 
         <style>
-*{
- margin:0;
-padding: 0;
-}
-.btn{
- background-color: #7d03ff !important;
- color:#ffffff !important;
-height: 60px !important;
-}
-.row{
- margin-top: 75px;
-}
-.jalg{
-height: 250px;
-background-color: #7d03ff !important;
-color: #ffffff !important;
-margin: 0;
-padding: 0;
+            *{
+                margin:0;
+                padding: 0;
+            }
+            .btn{
+                background-color: #7d03ff !important;
+                color:#ffffff !important;
+                height: 60px !important;
+            }
+            .row{
+                margin-top: 75px;
+            }
+            .jalg{
+                height: 250px;
+                background-color: #7d03ff !important;
+                color: #ffffff !important;
+                margin: 0;
+                padding: 0;
                 
-}
-.tekst {
-
-}
-.sub-text {
-  color: #666666; /* Hall värv */
-  font-size: 1rem; /* Vaiksem fondi suurus */
-  margin-top: -10px;
-}
-.lorem {
-  overflow-wrap: break-word;
-  word-wrap: break-word;
-  max-width: 300px;
-  font-size: 0.9rem;
-}
-.palk {
-  border-radius: 25px;
-background: #8a2be2;
-padding: 20px;
-width: 140px;
-height: 70px;
-color: white;
-}
-.pilt {
-  float:right;
-  border-radius: 8px;
-  width: 400px;
-  height: auto;
-  margin-top: -200px;
- }
-
- .tekstt {
-  text-align: center;
-  line-height: 2;
-  font-weight: bold;
-  margin-bottom: 5px;
-  margin-top: 40px;
-
- }
- .hh {
-  text-align: center;
-  margin-top: 0px;
- }
+            }
         </style>
 
     </head>
@@ -91,59 +49,84 @@ color: white;
                     <a class="navbar-brand text-dark" href="#"></a>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <a class="navbar-brand" href='#'>
-                            HiimoNarep.ee
+                            narep.ee
                         </a>
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link active" href="yl4cv.php">Avaleht</a>
+                                <a class="nav-link active" href="cv4php.php">Avaleht</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="tehtud.php">tehtud tööd</a>
+                                <a class="nav-link" href="tehtud.php?leht=tehtud">Tehtud tööd</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link " href="oskused.php">Oskused</a>
+                                <a class="nav-link " href="oskused.php?leht=oskused">Oskused</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="kontakt.php">Kontakt</a>
+                                <a class="nav-link" href="kontaktid.php?leht=konntakt">Kontakt</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="admin.php">Admin</a>
+                                <a class="nav-link" href="admin.php?leht=admin">Admin</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
-
-            <div class="container mt-4">
-  <div class="row justify-content-center"> 
-    <div class="col-md-8"> 
-      <h1 class="tekst">Hei, olen Hiimo</h1>
-      <p class="sub-text">Veebiarendaja</p> 
-      <p class="lorem">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque deleniti consequatur, saepe facilis eos voluptate eum magni odio dicta quo debitis temporibus sapiente rem illo, aliquid quas quaerat minima at.</p>
-      <p class="palk"> <i class="fa-solid fa-arrow-right"></i> Palka mind</p>
-      <img class="pilt" src="https://as2.ftcdn.net/v2/jpg/00/88/53/89/1000_F_88538986_5Bi4eJ667pocsO3BIlbN4fHKz8yUFSuA.jpg" alt="Pilt"> 
-    </div>
-  </div>
-</div>
+            <?php
+                if (isset($_GET['leht'])) {
+                    $leht = $_GET['leht'];
+                    if (is_file($leht.'.php')) {
+                        include($leht.'.php');
+                    } else {
+                        echo "<div class='alert alert-danger' role='alert'>Sellist lehte ei eksisteeri</div>";
+                    }
+                }
+            ?>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card border-0">
+                        <div class="card-body">
+                            <?php
+                                $allikas = "tekstid.txt";
+                                $fail = fopen($allikas, "r");
+                                $tekst = fread($fail, filesize($allikas));
+                                if ($tekst !== false) {
+                                    $tekstimassiiv = explode("\n", $tekst);
+                                }
+                                echo "<h1 class='display-4 fw-bold'>".$tekstimassiiv[0]."</h1>
+                                <h3 class='card-title text-muted'>".$tekstimassiiv[1]."</h3>
+                                <p class='card-text mb-4 mt-3 text-muted h5'>".$tekstimassiiv[2]."</p>";
+                            ?>
+                            <button type="button" class='btn'>Palka mind <i class="fa-regular fa-paper-plane"></i></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 mt-5">
+                    <img src=mees.jpg class="object-fit-contain rounded-circle img-fluid">
+                </div>
+            </div>
 
             <div class="row text-center">
-                <h1>Minust</h1>
-                <p class="text-muted">Minu parimad saavutused<p>
+                <h1>
+                    <?php
+                    echo $tekstimassiiv[3];
+                    ?>
+                </h1>
+                <p class="text-muted"><?php echo $tekstimassiiv[4]; ?></p>
             </div>
 
             <div class="row">
                 <div class="col-sm-6">
-                    <img src=https://cdn.geekwire.com/wp-content/uploads/2012/02/nerd-bigstock_Extreme_Computer_Nerd_1520708.jpg class="img-fluid">
+                    <img src=nerd.jpg class="img-fluid">
                 </div>
 
                 <div class="col-sm-6">
                     <div class="card border-0">
                         <div class="card-body">
-                            <p class="card-text mb-4 mt-3 text-muted h5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <p class="card-text mb-4 mt-3 text-muted h5"><?php echo $tekstimassiiv[5]; ?></p>
                             <div class="row">
                                 <div class="col-sm-4 text-center">
                                     <span class="kogemus">
-                                        <h3>3+</h3>
+                                        <h3>1+</h3>
                                     </span>
                                     <p class='text-muted'>aastat<br>kogemust</p>
                                 </div>
@@ -151,19 +134,19 @@ color: white;
                                     <span class="projekt">
                                         <h3>2+</h3>
                                     </span>
-                                    <p class='text-muted'>projekti<br>tehtud</p>
+                                    <p class='text-muted'>projekti<br>lõpetanud</p>
                                 </div>
                                 <div class="col-sm-4 text-center">
                                     <span class="ettevote">
-                                        <h3>0+</h3>
+                                        <h3>0</h3>
                                     </span>
-                                    <p class='text-muted'>ettevottes<br>tootanud</p>
+                                    <p class='text-muted'>ettevõttes<br>töötanud</p>
                                 </div>
                             </div>
 
                             <div class="row">
                                 <div class="col-sm-6 text-center">
-                                    <button class=btn>Lae alla CV <i class="fa-solid fa-download"></i></button>
+                                    <button class=btn>lae alla CV <i class="fa-solid fa-download"></i></button>
                                 </div>
                             </div>
                         </div>
@@ -176,14 +159,11 @@ color: white;
                 <p class="small text-center text-light"><i class="fa-solid fa-copyright"></i> narep</p>
             </div>
         </div>
-
-
         <script
             src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
             integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
             crossorigin="anonymous"
         ></script>
-
         <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
             integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
