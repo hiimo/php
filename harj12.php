e58cdbde8cbbb9781fa1f4517cf8b5189013d596
hiimo <115462482+hiimo@users.noreply.github.com>
2024-06-18 01:24:24 +0300

    Update harj12.php

--- harj12.php
+++ harj12.php
@@ -1,74 +1,78 @@
 <!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>harj 12</title>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-</head>
-<body>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
 
-<div class="container">
-<h1>Auto sõiduaeg</h1>
-    <form method="get">
-        <label for="start">start (hh:mm):</label>
-        <input type="text" id="start" name="start" maxlength="5" required pattern="\d{2}:\d{2}">
-        <br>
-        <label for="end">lopp (hh:mm):</label>
-        <input type="text" id="end" name="end" maxlength="5" required pattern="\d{2}:\d{2}">
-        <br>
-        <button type="submit">arvuta aeg</button>
-    </form>
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
 
-    <?php
-    if (!empty($_GET["start"]) && $_GET["end"]) {
-        $algus = $_GET["start"];
-        $lopp = $_GET["end"];
-        if (strlen($algus) < 5 || strlen($lopp) < 5) {
-            echo "<p>peab olema rohkem kui 5</p>";
-        } else {
-            list($tund, $minut) = explode(":", $algus);
-            list($tund2, $minut2) = explode(":", $lopp);
-            $startstamp = $tund * 60 + $minut;
-            $endstamp = $tund2 * 60 + $minut2;
-            $diffminutes = ($endstamp - $startstamp + 1440) % 1440;
+    <body>
+        <div class="container">
+            <h1>Harjutus</h1>
+            <?php
+                echo "<form action='#' method='get'>
+                    <input type='text' name='start' id='start' />
+                    <input type='text' name='lopp' id='lopp' />
+                    <input type='submit' value='Saada' />
+                    </form>";
 
-            $hours = floor($diffminutes / 60);
-            $minutes = $diffminutes % 60;
-            echo "<p>Sõiduaeg: $hours tundi ja $minutes minutit.</p>";
-        }
-    }
-    ?>
- <?php
-$fail="tootajad.csv";
-$mpalk= 0;
-$npalk= 0;
-$marv= 0;
-$narv= 0;
-$mmax= 0;
-$nmax= 0;
-$csv = fopen($fail, "r") or die("pole faili");
-while(feof($csv)){
-$rida = fgetcsv($csv, filesize($fail),";");
+                if (isset($_GET['start']) && isset($_GET['lopp'])){
+                    if (strlen($_GET['start']) == 5 && $_GET['start'][3] == ':' && str_contains($_GET['lopp'], ':') && $_GET['start'][3] == ':' && strlen($_GET['lopp']) == 5){
+                        $ex1 = explode(":", $_GET['start']);
+                        $ex2 = explode(":", $_GET['lopp']);
+                        echo $ex1[0]-$ex2[0]."tundi ja ".$ex1[1]-$ex2[1]."minutit";
+                    }
+                }
+                echo '<br>';
+                $fail = 'tootajad.csv';
+                $mp = 0;
+                $np = 0;
+                $ma = 0;
+                $na = 0;
+                $maxm = 0;
+                $maxn = 0;
 
-if ($rida[1] == "m"){
-    $mpalk += $rida[2];
-    $marv++;
-    $mmax = max($mmax, $rida[2]);
-}
-else{
-    $npalk += $rida[2];
-    $narv++;
-    $nmax = max($nmax, $rida[2]);
-}
-}
-fclose($csv);
-$km= $marv > 0 ? round($mpalk / $marv) : 0;
-$kn= $narv > 0 ? round($npalk / $narv) : 0;
-echo "Meeste keskmine palk: $km<br>Naiste kesmine palk: $kn<br>";
-echo "Meeste suurim palk on: $mmax<br>Naiste suurim palk on<br>";
-?>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
+                $csv = fopen($fail, 'r') or die('Ei saa avada');
+                while(!feof($csv)){
+                    $rida = fgetcsv($csv, 0, ';');
+                    if ($rida[1] == 'm'){
+                        $mp += $rida[2];
+                        $ma ++;
+                        $maxm = max($maxm, $rida[2]);
+                    }
+                    if ($rida[1] == 'n'){
+                        $np += $rida[2];
+                        $na ++;
+                        $maxn = max($maxn, $rida[2]);
+                    }
+                }
+                fclose($csv);
+                $keskm = $ma > 0 ? round($mp / $ma) : 0;
+                $keskn = $na > 0 ? round($np / $na) : 0;
+                echo "Meeskonnas on ".$ma." meest keskmine palk on ".$keskm." suurim palk on ".$maxm."<br>";
+                echo "Meeskonnas on ".$na." naist keskmine palk on ".$keskn." suurim palk on ".$maxn;
+            ?>
+        </div>
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
 </html>
